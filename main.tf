7870f94840f93a6682559df2d6c9400abd35f12d
jyo111 <60911355+jyo111@users.noreply.github.com>
2021-12-03 15:29:32 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region = "us-east-2"
+  access_key = "******"
+  secret_key = "******"
+}
+
 resource "aws_instance" "deleteme" {
   ami           = "ami-0629230e074c580f2"
   instance_type = var.instance_type
